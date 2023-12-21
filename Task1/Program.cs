49b7ad46431f9ce88765dab26ec50246f81f3b4b
Magamed2009 <muhammadmkr6@gmail.com>
2023-12-21 15:52:08 +0500

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,44 @@
+﻿var about = new Teacher("asd","jvvdj",4);
+
+System.Console.WriteLine(about.Teach());
+
+System.Console.WriteLine(about.SetExperience(12));
+
+System.Console.WriteLine(about.GetExperience());
+
+class Teacher
+{
+  private string name;
+  private string subject;
+  private int experience;
+  public string Name {get; set;}
+  public string Subject {get; set;}
+  public int Experience {get; set;}
+  public string Teach()
+  {
+    return $"{Name} is teaching {Subject}";
+  }
+  public string SetExperience(int years)
+  {
+    return $"{Experience}";
+  }
+  public string GetExperience()
+  {
+    return $"{Experience}";
+  }
+  public Teacher()
+  {
+
+  }
+  public Teacher(string a,string b)
+  {
+    Name=a;
+    Subject=b;
+  }
+   public Teacher(string a,string b,int c)
+  {
+    Name=a;
+    Subject=b;
+    Experience=c;
+  }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,44 @@
+﻿var about = new Study("asd",2,4);
+
+System.Console.WriteLine(about.Teach());
+
+System.Console.WriteLine(about.SetExperience(12));
+
+System.Console.WriteLine(about.GetExperience());
+
+class Study
+{
+  private string name;
+  private int age;
+  private int grade;
+  public string Name {get; set;}
+  public int Age {get; set;}
+  public int Grade {get; set;}
+  public string Teach()
+  {
+    return $"{Name} is teaching {Age}";
+  }
+  public string SetExperience(int newGrade)
+  {
+    return $"{Grade}";
+  }
+  public string GetExperience()
+  {
+    return $"{Grade}";
+  }
+  public Study()
+  {
+
+  }
+  public Study(string a,int b)
+  {
+    Name=a;
+    Age=b;
+  }
+   public Study(string a,int b,int c)
+  {
+    Name=a;
+    Age=b;
+    Grade=c;
+  }
+}
